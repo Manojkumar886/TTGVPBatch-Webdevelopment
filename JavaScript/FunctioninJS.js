f3ff40cb0a7bd007c7c3fe082c9f7f9ca4bafd2d
Manojkumar886 <maddymano434@gmail.com>
2022-12-22 11:58:02 +0530

    Array Basic and innerhtml is done

---
+++ JavaScript/FunctioninJS.js
@@ -0,0 +1,19 @@
+function First()
+{
+    alert("Welcome to Java Script Function Normal Process")
+}
+
+function details(name,age)
+{
+    alert("My name is "+name+": my age is "+age)
+}
+
+var process=()=>
+{
+    document.writeln("This is fat arrow function using by java script")
+}
+
+const calculation=(a,b)=>
+{
+    return a*b;
+}
