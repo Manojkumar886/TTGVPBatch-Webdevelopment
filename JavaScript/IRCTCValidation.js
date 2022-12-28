46423aa5b51982bfbdff8607f9637f831d97edfb
Manojkumar886 <maddymano434@gmail.com>
2022-12-28 10:55:36 +0530

    Js is Over

--- JavaScript/IRCTCValidation.js
+++ JavaScript/IRCTCValidation.js
@@ -3,8 +3,19 @@ let Action=()=>
     let a=document.forms['irctc'].fno.value;
     let b=document.forms['irctc'].Svalue.value;
     let c=document.forms['irctc'].pin.value;
-    let active=true;
+    let d=document.forms['irctc'].email1.value;
 
+    let active=true;
+    if(d.length<=0)
+    {
+        document.getElementById('edetails').innerHTML="Email id should't empty";
+        active=false;
+    }
+    else if(!((/^(?=.*[0-9])(?=.+[@.])[a-z0-9@.]+$/).test(d)))
+    {
+        document.getElementById('edetails').innerHTML="Email id used by @ . symbol";
+        active=false;
+    }
     if(a.length<=0)
     {
         document.getElementById('fdetails').innerHTML="Flat input should't empty";
